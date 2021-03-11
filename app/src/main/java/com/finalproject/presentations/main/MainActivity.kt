f93f7a7a531d13098bccc92abfa62460f6baa3b2
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-03-11 08:08:49 +0700

    Splash Screen Completed Fragment

--- app/src/main/java/com/finalproject/presentations/main/MainActivity.kt
+++ app/src/main/java/com/finalproject/presentations/main/MainActivity.kt
@@ -2,11 +2,53 @@ package com.finalproject.presentations.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.finalproject.R
+import com.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupNav()
+
     }
+
+    private fun setupNav() {
+        navController.addOnDestinationChangedListener { _, destination, _ -> //Untuk membuat settingan hide atau show bottomnavigation view pada layout activity
+            when (destination.id) {
+                R.id.splashFragment -> hideBottomNav()
+                R.id.loginFragment -> hideBottomNav()
+            }
+        }
+
+    }
+
+    //Function untuk memunculkan bottom Navigation View
+    private fun showBottomNav() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+
+    }
+
+    //Function untuk menyembunyikan bottom Navigation View
+    private fun hideBottomNav() {
+        binding.bottomNavigationView.visibility = View.GONE
+
+    }
+
 }
---
+++ app/src/main/java/com/finalproject/presentations/splash/SplashFragment.kt
@@ -0,0 +1,42 @@
+package com.finalproject.presentations.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.finalproject.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_loginFragment)
+        }
+    }
+
+    companion object {
+
+        fun newInstance() = SplashFragment()
+
+    }
+}
