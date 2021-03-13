94773dd1248431f4511056290f3516250956fcf5
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-03-13 21:07:24 +0700

    Layout Menu Account Fragment Changed

--- app/src/main/java/com/finalproject/presentations/employee/claim/glasses/ClaimGlassesFragment.kt
+++ app/src/main/java/com/finalproject/presentations/employee/claim/glasses/ClaimGlassesFragment.kt
@@ -1,17 +1,13 @@
-package com.finalproject.presentations.employee.account
+package com.finalproject.presentations.employee.claim.glasses
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import com.finalproject.R
 
-class AccountFragment : Fragment() {
-
+class ClaimGlassesFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,11 +16,11 @@ class AccountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        return inflater.inflate(R.layout.fragment_claim_glasses, container, false)
     }
 
     companion object {
-        @JvmStatic
-        fun newInstance() = AccountFragment()
+
+        fun newInstance() = ClaimGlassesFragment()
     }
 }
--- app/src/main/java/com/finalproject/presentations/employee/history/HistoryFragment.kt
+++ app/src/main/java/com/finalproject/presentations/employee/history/HistoryFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
 import com.finalproject.R
 
 class HistoryFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
