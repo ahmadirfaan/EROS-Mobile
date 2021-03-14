97fd81d1950c8956d78b62d4e3f52afcc6eb2b8d
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-03-14 20:43:49 +0700

    History Layout Page Fragment

---
+++ app/src/main/java/com/finalproject/presentations/employee/history/onprogress/HistoryOnProgressFragment.kt
@@ -0,0 +1,44 @@
+package com.finalproject.presentations.employee.history.onprogress
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.finalproject.R
+import com.finalproject.databinding.FragmentHistoryOnprogressBinding
+
+class HistoryOnProgressFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryOnprogressBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistoryOnprogressBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigate(R.id.action_historyOnProgressFragment_to_homeEmployeeFragment2)
+        }
+        binding.tvMoveFragmentHistorySuccess.setOnClickListener {
+            findNavController().navigate(R.id.action_historyOnProgressFragment_to_historySuccessFragment2)
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = HistoryOnProgressFragment()
+    }
+}
---
+++ app/src/main/java/com/finalproject/presentations/employee/history/success/HistorySuccessFragment.kt
@@ -0,0 +1,46 @@
+package com.finalproject.presentations.employee.history.success
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.finalproject.R
+import com.finalproject.databinding.FragmentHistorySuccessBinding
+
+class HistorySuccessFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistorySuccessBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistorySuccessBinding.inflate(layoutInflater)
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigate(R.id.action_historySuccessFragment2_to_homeEmployeeFragment)
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvMoveFragmentHistoryOnprogress.setOnClickListener {
+            findNavController().navigate(R.id.action_historySuccessFragment2_to_historyOnProgressFragment)
+        }
+    }
+
+
+    companion object {
+
+        fun newInstance() = HistorySuccessFragment()
+
+    }
+}
